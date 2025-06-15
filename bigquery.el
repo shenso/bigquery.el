888a8f5107943138601e0d79d5dfae66c4ac2957
Shawn Henson <shawn@shenso.name>
2025-06-15 14:59:26 -0400

    Add GPLv3 license

--- bigquery.el
+++ bigquery.el
@@ -3,10 +3,23 @@
 ;; Copyright (C) 2025 Shawn Henson
 
 ;; Author: Shawn Henson <shawn@shenso.name>
-;; Version: 0.1
+;; Version: 0.1.0
 ;; Package-Requires: ()
-;; Keywords: sql, bigquery
+;; Keywords: sql, bigquery, gcloud, google cloud
 ;; URL: https://github.com/shenso/bigquery.el
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
